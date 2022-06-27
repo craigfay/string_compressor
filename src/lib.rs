b55cc755a5c42bacff77d3b8f8723d1588e9e5c8
craigfay <craigfay1@gmail.com>
2022-06-26 23:32:34 -0500

    Use little endian, trim zeros

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ use base64;
 /// a "hasher", because it optimizes for small hash sizes, but two
 /// instances won't yield the same hashed values for a given input
 /// string unless the order of hash operations is identical.
-#[derive(Default)]
 pub struct StringCompressor {
     expansion_map: HashMap<String, String>,
     compression_map: HashMap<String, String>,
@@ -17,13 +16,26 @@ pub struct StringCompressor {
 impl StringCompressor {
     fn increment_key(&mut self) -> String {
         self.counter += 1;
-        let bytes = (self.counter - 1).to_ne_bytes();
+        let mut bytes = (self.counter - 1).to_le_bytes().to_vec();
+
+        // Trimming leading-zero bytes to reduce size
+        while let Some(byte) = bytes.pop() {
+            if byte != 0 || bytes.len() == 0 {
+                bytes.push(byte);
+                break;
+            }
+        }
+
         base64::encode_config(&bytes, base64::URL_SAFE_NO_PAD)
     }
 
     /// Create a new `StringCompressor` instance.
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            counter: 0,
+            expansion_map: HashMap::new(),
+            compression_map: HashMap::new(),
+        }
     }
 
     /// Create a compressed alias for a given `String`.
@@ -47,4 +59,17 @@ impl StringCompressor {
             None => None,
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::StringCompressor;
+
+    #[test]
+    fn leading_zeros_are_trimmed() {
+        let mut compressor = StringCompressor::new();
+        let alias = compressor.compress("hello!".to_string());
+        assert_eq!(alias, "AA");
+    }
+
 }
